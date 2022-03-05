6df6d8f86e0040c6bcae72f7a9d43647b98d7360
Jonathon Squire <jgsquire27@gmail.com>
2022-03-05 15:45:13 -0700

    added task input field and add button
    changed several variable names
    added ability to name lists
    started on create new task function

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
 function createNewList () {
   const listContainer = document.getElementById("list-container");
-  const div = document.createElement("div");
-  const list = document.createElement("ul");
-  const listChild = document.createElement("li");
-  div.appendChild(list);
-  list.appendChild(listChild);
-  listContainer.appendChild(div)
+  const listTextInput = document.getElementById("list-text-input").value;
+  const listDiv = document.createElement("div");
+  const listTaskContainer = document.createElement("div");
+  const listTitle = document.createElement("h3");
+  const listTaskInput = document.createElement("input");
+  const listTaskInputButton = document.createElement("button");
+  listTitle.innerHTML = listTextInput;
+  listTaskInputButton.innerHTML = "Add Task";
+  listTaskInputButton.addEventListener('click', createNewTask)
+  listDiv.appendChild(listTitle);
+  listDiv.appendChild(listTaskInput);
+  listDiv.appendChild(listTaskInputButton);
+  listDiv.appendChild(listTaskContainer);
+  // list.appendChild(listChild);
+  listContainer.appendChild(listDiv);
 
+}
+
+
+
+function createNewTask (taskTitleString) {
+
+
+  const taskTitle = document.createElement("h4");
+//I need to 2 things in this function
+// 1: give the task the title value  ie ketchup or bread etc...
+// 2: I need to put that in the actual task list (listTaskContainer)
 }
 
 
