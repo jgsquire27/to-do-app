d24215c4bd481f12e591621ff75c2c6d561a3188
Jonathon Squire <jgsquire27@gmail.com>
2022-03-05 14:03:12 -0700

    added Create list function

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-const lists = {
-  
+function createNewList () {
+  const listContainer = document.getElementById("list-container");
+  const div = document.createElement("div");
+  const list = document.createElement("ul");
+  const listChild = document.createElement("li");
+  div.appendChild(list);
+  list.appendChild(listChild);
+  listContainer.appendChild(div)
+
 }
+
+
