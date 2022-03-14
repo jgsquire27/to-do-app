5fd768d2e6603df976357cb22192e9856e77e3c6
Jonathon Squire <jgsquire27@gmail.com>
2022-03-14 17:15:42 -0600

    now adds tasks to lists, added comments

--- script.js
+++ script.js
@@ -1,29 +1,50 @@
-function createNewList () {
-  const listContainer = document.getElementById("list-container");
-  const listTextInput = document.getElementById("list-text-input").value;
-  const listDiv = document.createElement("div");
-  const listTaskContainer = document.createElement("div");
-  const listTitle = document.createElement("h3");
-  const listTaskInput = document.createElement("input");
-  const listTaskInputButton = document.createElement("button");
-  listTitle.innerHTML = listTextInput;
-  listTaskInputButton.innerHTML = "Add Task";
-  listTaskInputButton.addEventListener('click', createNewTask)
-  listDiv.appendChild(listTitle);
-  listDiv.appendChild(listTaskInput);
-  listDiv.appendChild(listTaskInputButton);
-  listDiv.appendChild(listTaskContainer);
-  // list.appendChild(listChild);
-  listContainer.appendChild(listDiv);
 
+//declares function createNewList
+function createNewList () {
+  //assigns element .list-container to listContainer\/ 
+  const listContainer = document.getElementById("list-container");
+  //assigns element .list-text-input value to listTextInput\/
+  const listTextInput = document.getElementById("list-text-input").value;
+  //creates a div element and assings it to listDiv const
+  const listDiv = document.createElement("div");
+  //creates a dvi element and assigns it to listTaskContainer variable
+  const listTaskContainer = document.createElement("div");
+  //creates a h3 element and assigns it to listTitle variable
+  const listTitle = document.createElement("h3");
+  //creates an input element and assigns it to listTaskInput variable
+  const listTaskInput = document.createElement("input");
+  //creates a button element and assigns it to listTaskInput variable
+  const listTaskInputButton = document.createElement("button");
+  //creates a button element and assigns it to listTaskInputButton variable
+
+  
+  listTitle.innerHTML = listTextInput;
+  //assigns listTextInput value to the innerHTML of listTitle
+  listTaskInputButton.innerHTML = "Add Task";
+  //assigns "add task" value to listTaskInputButton
+  listTaskInputButton.addEventListener('click', () => createNewTask(listTaskInput, listTaskContainer));
+  //when listTaskInputButton is clicked(event listener) it runs the function createNewTask
+  listDiv.appendChild(listTitle);
+  //adds child listTitle to parent listDiv
+  listDiv.appendChild(listTaskInput);
+  //adds child listTaskInput to parent listDiv
+  listDiv.appendChild(listTaskInputButton);
+  //adds child listTaskInputButton to parent listDiv
+  listDiv.appendChild(listTaskContainer);
+  //adds child listTaskContainer to parent listDiv
+  listContainer.appendChild(listDiv);
+  //adds child listDiv to parent listContainer
 }
 
 
 
-function createNewTask (taskTitleString) {
-
-
+function createNewTask (listTaskInput, taskContainer) {
+  const taskValue = listTaskInput.value;
   const taskTitle = document.createElement("h4");
+  taskTitle.innerHTML = taskValue;
+  taskContainer.appendChild(taskTitle);
+  const strikeThroughClass = document.getElementById("eventID");
+  strikeThroughClass.classList.add("eventClass");
 //I need to 2 things in this function
 // 1: give the task the title value  ie ketchup or bread etc...
 // 2: I need to put that in the actual task list (listTaskContainer)
